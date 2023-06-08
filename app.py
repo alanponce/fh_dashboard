6af54df1d6dfc02d16e0e688085212d2bb09429f
Alan Ponce <alanponc@hotmail.com>
2023-06-08 00:15:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ with st.sidebar:
     #    df_filter = df_filter[df_filter['UserEmploymentStatus'] == employmentStatus_filter]
     #    filters_text.append("Employment status: " + employmentStatus_filter) 
      
-     st.table(engagement_list.head(10))
+    st.table(engagement_list.head(10))
 
     #this data is used in both plots
     engagement_list = get_engagement_list(df = df_filter, lookback = int(lookback), from_date=pd.Timestamp(str(start_date)))
